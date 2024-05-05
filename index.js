6f1794df9903d38931f24747d68ec6e61db1d9a4
sudharshana <sudhubalu04@gmail.com>
2024-05-05 12:32:14 +0530

    Two routers

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import router from './router/index.js'
+import userRouter from './router/users.js'
+
 import express from "express"
 
 const app = express()
 const port=8000
 
 app.get('/',(req,res)=>{
+
     res.send("hello sudhu")
 })
 
+
 app.use(router)
+app.use("/family",userRouter)
+
+
 
 app.listen(port,()=>{
     console.log("Server is running");
--- router/index.js
+++ router/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import userRouter from './users.js'
 const router=express.Router()
 
 router.get('/about',(req,res)=>{
@@ -7,5 +8,21 @@ router.get('/about',(req,res)=>{
         name:"sudhu"
     })
 })
+
+
+router.get('/about/:user',(req,res)=>{
+    res.json({
+        id:1,
+        name:req.params.user
+    })
+
+})
+
+router.use("/family",userRouter)
+router.get('*',(req,res)=>
+{
+    res.send("astriek")
+})
+
 
 export default router;
